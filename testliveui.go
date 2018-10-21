19f57d1f776d14f26da8791719b723a4b00de15b
The third meow <jsheaandcats@gmail.com>
2018-10-21 18:18:39 +1300

    work test 'til usable if not great to look at

--- testliveui.go
+++ testliveui.go
@@ -7,15 +7,27 @@ import (
 )
 
 func main() {
+	instrings := make([]string, 0)
+
 	writer := ui.New()
 	writer.Start()
 	for i := 0; i < 14; i++ {
-		fmt.Fprintf(writer, "\n%d", i)
-		fmt.Fprintf(writer, "\n>>>")
+		fmt.Fprintf(writer, "\n%d ", i)
+		fmt.Fprintf(writer, ">>>")
+		go func() {
+			var s string
+			_, _ = fmt.Scanf("%s\n", &s)
+			instrings = append(instrings, s)
+		}()
 		if i%4 == 3 {
-			fmt.Printf("\ntimer %d is done\n\n", i)
+			fmt.Fprintf(writer.Bypass(), "\ntimer %d is done", i)
 		}
 		time.Sleep(time.Second)
 	}
 	writer.Stop()
+
+	fmt.Println("")
+	for _, e := range instrings {
+		fmt.Println(e)
+	}
 }
