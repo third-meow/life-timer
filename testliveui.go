3fcc1324501b2442f51cd02febb2061c448f9c08
The third meow <jsheaandcats@gmail.com>
2018-11-03 12:54:38 +1300

    make test work

--- testliveui.go
+++ testliveui.go
@@ -6,28 +6,39 @@ import (
 	ui "uilive"
 )
 
+func dispAll(w *ui.Writer, i int) {
+	fmt.Fprintf(w, "\n%d ", i)
+	fmt.Fprintf(w, ">>>")
+	if i%4 == 3 {
+		fmt.Fprintf(w.Bypass(), "\ntimer %d is done", i)
+	}
+}
+
+func takeInput(w *ui.Writer, i int, out *[]string) {
+	var s string
+	fmt.Scanf("%s\n", &s)
+	*out = append(*out, s)
+	dispAll(w, i)
+}
+
 func main() {
 	instrings := make([]string, 0)
 
 	writer := ui.New()
 	writer.Start()
 	for i := 0; i < 14; i++ {
-		fmt.Fprintf(writer, "\n%d ", i)
-		fmt.Fprintf(writer, ">>>")
-		go func() {
-			var s string
-			_, _ = fmt.Scanf("%s\n", &s)
-			instrings = append(instrings, s)
-		}()
-		if i%4 == 3 {
-			fmt.Fprintf(writer.Bypass(), "\ntimer %d is done", i)
-		}
-		time.Sleep(time.Second)
+		dispAll(writer, i)
+		go takeInput(writer, i, &instrings)
+		time.Sleep(time.Second * 8)
 	}
 	writer.Stop()
 
 	fmt.Println("")
 	for _, e := range instrings {
 		fmt.Println(e)
+	}
+	for i := 0; i < 3; i++ {
+		var nothing string
+		fmt.Scanln(&nothing)
 	}
 }
